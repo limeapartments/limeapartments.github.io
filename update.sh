12f9209483ad28da89038569dd28adcefa1f2230
Chance Hudson <jchancehud@gmail.com>
2019-03-01 17:09:55 -0600

    Fix matching commands

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+BASEDIR="$(cd "$(dirname "$0")"; pwd)";
+
+# Assert existence of mktemp
+which mktemp
+which npm
+which git
+
+# Get a path
+TMPDIR=$(mktemp -d)
+
+# Clone into path
+cd $TMPDIR
+
+git clone https://github.com/limeapartments/website.git
+cd website
+npm install
+npm run build:production
+
+# Remove old site content, keep repo files
+ls -Ad "$BASEDIR/"* \
+| grep -v /CNAME \
+| grep -v /update.sh \
+| grep -v /README.md \
+| grep -v /.git \
+| grep -v /.gitignore \
+| xargs rm -r
+
+# Copy new build files in
+cp -r build/. $BASEDIR
